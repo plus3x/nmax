7c7c5040cbb74311633b1e872e0e15aee3135dde
Vladislav Petrov <electronicchest@gmail.com>
2016-03-28 23:39:35 +0300

    Add tests

---
+++ test/nmax_test.rb
@@ -0,0 +1,21 @@
+require_relative 'test_helper'
+
+describe NMax do
+  def nmax(command = '', previous_command: nil)
+    `#{previous_command}#{Bundler.root}/bin/nmax #{command}`
+  end
+
+  describe 'CLI' do
+    it 'shows --version' do
+      nmax('--version').must_include NMax::VERSION
+    end
+
+    it 'shows --help' do
+      nmax('--help').must_include 'nmax'
+    end
+  end
+
+  describe 'calculation' do
+
+  end
+end
