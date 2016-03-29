b18fd9d4982548ee16530b3a9af4bfe61511d546
Vladislav Petrov <electronicchest@gmail.com>
2016-03-29 12:47:40 +0300

    Optimize algorithm for less memory usage

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,7 +1,36 @@
 module NMax
   autoload :VERSION, 'nmax/version'
 
-  def self.get(stream:, n:)
-    stream.grep(/\d+/) { |d| d[/(\d+)/, 1].to_i }.sort_by { |a, b| b <=> a }[0..(n - 1)]
+  def self.get(stdin:, count:)
+    chars = stdin.each_char
+    grouped_numbers = chars.chunk { |c| c =~ /\d/ }
+
+    numbers = grouped_numbers.inject([]) do |memo, (_, digits)|
+      memo << digits.join.to_i
+      memo = memo.sort.last(count) if memo.size > count
+      memo
+    end
+
+    numbers.sort.last(count)
   end
+
+  # Not beautiful, but faster
+  # def self.get(stdin:, count:)
+  #   number_str = ''
+  #   numbers = []
+
+  #   stdin.each_char do |c|
+  #     if c =~ /\d/
+  #       number_str << c
+  #     elsif !number_str.empty?
+  #       numbers << number_str.to_i
+  #       numbers = numbers.sort.last(count) if numbers.size > count
+  #       number_str.clear
+  #     end
+  #   end
+
+  #   numbers << number_str.to_i unless number_str.empty?
+
+  #   numbers.sort.last(count)
+  # end
 end
--- test/nmax_test.rb
+++ test/nmax_test.rb
@@ -1,20 +1,24 @@
 require_relative 'test_helper'
+require 'tempfile'
 
 describe NMax do
-  def nmax(command = '', previous_command: nil)
-    `#{previous_command}#{Bundler.root}/bin/nmax #{command}`
+  it 'should get 2 max numbers from readed file' do
+    assert_equal `cat #{Bundler.root}/test/fixtures/text_with_numbers.txt | #{Bundler.root}/bin/nmax 2}`, "22\n30\n"
   end
 
-  describe 'CLI' do
-    it 'shows --version' do
-      nmax('--version').must_include NMax::VERSION
-    end
-
-    it 'shows --help' do
-      nmax('--help').must_include 'nmax'
-    end
+  def memstats
+    `ps -o rss= #{Process.pid}`.strip.to_i
   end
 
-  describe 'calculation' do
+  it 'should not load file to process memory' do
+    file = Tempfile.new('tmp')
+    file.write(('0'..'z').to_a.*(50_000).sample(1_000_000).join)
+    file.rewind
+
+    starting_memory = memstats
+
+    NMax.get(stdin: file, count: 5)
+
+    assert memstats < starting_memory + 6000, 'Calculation has consumed more than 6MB'
   end
 end
