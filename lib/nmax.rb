180c67fbf2e08cec6c4654ca1718d3b4242bdbc2
Vladislav Petrov <electronicchest@gmail.com>
2019-02-06 12:59:08 +0300

    Focus allocation size for each Ruby testing versions

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -3,36 +3,43 @@
 module NMax
   autoload :VERSION, 'nmax/version'
 
-  def self.get(stdin:, count:)
-    chars = stdin.each_char
-    grouped_numbers = chars.chunk { |c| c =~ /\d/ }
-
-    numbers = grouped_numbers.inject([]) do |memo, (_, digits)|
-      memo << digits.join.to_i
-      memo = memo.sort.last(count) if memo.size > count
-      memo
+  class << self
+    def get(stdin:, count:, method: :faster)
+      send(method, stdin: stdin, count: count)
     end
 
-    numbers.sort.last(count)
+    private
+
+    def beautiful(stdin:, count:)
+      chars = stdin.each_char
+      grouped_numbers = chars.chunk { |c| c =~ /\d/ }
+
+      numbers = grouped_numbers.inject([]) do |memo, (_, digits)|
+        memo << digits.join.to_i
+        memo = memo.sort.last(count) if memo.size > count
+        memo
+      end
+
+      numbers.sort.last(count)
+    end
+
+    def faster(stdin:, count:)
+      number_str = String.new
+      numbers = []
+
+      stdin.each_char do |c|
+        if c =~ /\d/
+          number_str << c
+        elsif !number_str.empty?
+          numbers << number_str.to_i
+          numbers = numbers.sort.last(count) if numbers.size > count
+          number_str.clear
+        end
+      end
+
+      numbers << number_str.to_i unless number_str.empty?
+
+      numbers.sort.last(count)
+    end
   end
-
-  # Not beautiful, but faster
-  # def self.get(stdin:, count:)
-  #   number_str = ''
-  #   numbers = []
-
-  #   stdin.each_char do |c|
-  #     if c =~ /\d/
-  #       number_str << c
-  #     elsif !number_str.empty?
-  #       numbers << number_str.to_i
-  #       numbers = numbers.sort.last(count) if numbers.size > count
-  #       number_str.clear
-  #     end
-  #   end
-
-  #   numbers << number_str.to_i unless number_str.empty?
-
-  #   numbers.sort.last(count)
-  # end
 end
--- test/nmax_test.rb
+++ test/nmax_test.rb
@@ -12,15 +12,55 @@ describe NMax do
     `ps -o rss= #{Process.pid}`.strip.to_i
   end
 
-  it 'should not load file to process memory' do
+  def should_alllocate_less_then_killobytes(size = 1000)
     file = Tempfile.new('tmp')
     file.write(('0'..'z').to_a.*(50_000).sample(1_000_000).join)
     file.rewind
 
     starting_memory = memstats
 
-    NMax.get(stdin: file, count: 5)
+    yield(file, count = 5)
 
-    assert memstats < starting_memory + 6000, 'Calculation has consumed more than 6MB'
+    consumed_memory = memstats - starting_memory
+
+    assert consumed_memory < size, "Calculation has consumed more than #{size}KB(#{consumed_memory}KB)"
+  end
+
+  describe 'faster' do
+    it 'should not load file to process memory' do
+      # Calculated statistically
+      allocation_size =
+        case RUBY_VERSION.to_f
+        when 2.1 then 8000
+        when 2.2 then 5000
+        when 2.3 then 3000
+        when 2.4 then 1100
+        when 2.5 then 2300
+        when 2.6 then 2100
+        end
+
+      should_alllocate_less_then_killobytes(allocation_size) do |file, count|
+        NMax.get(stdin: file, count: count, method: :faster)
+      end
+    end
+  end
+
+  describe 'beautiful' do
+    it 'should not load file to process memory' do
+      # Calculated statistically
+      allocation_size =
+        case RUBY_VERSION.to_f
+        when 2.1 then 7500
+        when 2.2 then 4500
+        when 2.3 then 3000
+        when 2.4 then 1200
+        when 2.5 then 2100
+        when 2.6 then 2600
+        end
+
+      should_alllocate_less_then_killobytes(allocation_size) do |file, count|
+        NMax.get(stdin: file, count: count, method: :beautiful)
+      end
+    end
   end
 end
